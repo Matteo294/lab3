d8ffb5b221475fe3b6e7f55806d5286eba56dc4f
elenaacinapura <elena.acinapura@gmail.com>
2020-11-02 16:15:08 +0100

    Analisi dati più seria, grafico comprensibile

--- Esp3/part1.py
+++ Esp3/part1.py
@@ -13,8 +13,10 @@ w0 = 1/(R*C)
 # ciclo sugli epsilon
 y = []      # vettore dove metterò 1/tau stimato
 eps = numpify([1e-4, 1e-3, 1e-2, 1e-1])
-x = eps/(1+eps)
+x = numpify(eps/(1+eps))
 n = len(eps)
+
+################ DATA ################
 
 def exp_func (t, k, a):
     return a*np.exp(-t*k) # k è 1/tau
@@ -22,13 +24,12 @@ def exp_func (t, k, a):
 for i in range(n):
     file = "Newdata/eps" + str(i+1) + ".csv"
     # load data
-    print(i)
     [t, V] = readCSV(file, skiprows=1)
     t = numpify(t) - t[0]
     # fit with exponential
     fit = curve_fit(exp_func, t, V)
     [k, a] = fit[0]
-    if(i==0):
+    if(i==-1):
         tpoints = np.linspace(0, 1)
         plt.scatter(t, V, label="Data")
         plt.plot(tpoints, exp_func(tpoints, k, a), label="Fit")
@@ -38,5 +39,14 @@ for i in range(n):
 
 y = numpify(y)
 
-plt.scatter(x*w0, y)
+################ MODEL ################
+x_teo = np.logspace(-6, -1)
+y_teo = x_teo*w0    # 1/tau = eps/(1+eps) *w0
+
+plt.semilogx(x_teo, y_teo, c='k', label="Data")
+plt.scatter(x, y, c="red", label="Model")
+plt.legend()
+plt.xlabel(r"$\frac{\epsilon}{1+\epsilon}$")
+plt.ylabel(r"$1/\tau$")
+plt.tight_layout()
 plt.show()
